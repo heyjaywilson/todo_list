2cf587d363359d80a68cb3b9130a2e84b4dbb35d
maeganwilson_ <maegan@maeganwilson.com>
2019-01-28 19:27:10 -0600

    Day 001: Starting project

---
+++ todo-list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo-list
+//
+//  Created by Maegan Wilson on 1/28/19.
+//  Copyright © 2019 MW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
