0c01834f9add40ad78032b13bf1bd31b635cd439
maeganwilson_ <maegan@maeganwilson.com>
2019-01-29 12:09:49 -0600

    Can now add todos

---
+++ todo-list/TodoList.swift
@@ -0,0 +1,27 @@
+//
+//  TodoList.swift
+//  todo-list
+//
+//  Created by Maegan Wilson on 1/28/19.
+//  Copyright © 2019 MW. All rights reserved.
+//
+
+import Foundation
+
+struct Todo {
+    var task: String
+    var complete:Bool = false
+}
+
+class TodoList {
+    private var todoList = [Todo]()
+    
+    func addTodo(todo: String) {
+        let newTodo = Todo(task: todo, complete: false)
+        todoList.append(newTodo)
+    }
+    
+    func returnList() -> Array<Todo> {
+        return todoList
+    }
+}
--- todo-list/ViewController.swift
+++ todo-list/ViewController.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var todoInput: UITextField!
+    var todos = TodoList()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        todoInput.delegate = self
+        todoInput.returnKeyType = .done
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        addToList(resign: false)
+        return true
+    }
+    
+    @IBAction func addTodo(_ sender: Any) {
+        addToList(resign: true)
+        
+    }
+    // table
+    
+    // helper functions
+    
+    func addToList(resign:Bool) {
+        todos.addTodo(todo: todoInput.text!)
+        print("\(todos.returnList())")
 
+        todoInput.text = ""
+        
+        if (resign == true){
+            todoInput.resignFirstResponder()
+        }
+    }
 }
 
